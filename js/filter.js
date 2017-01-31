f2fa02ab79cdf34e5b84a70794adcfea1062b741
Nakada Takumi <usrNeko@kerorin.local>
2017-01-31 12:29:25 +0900

    refactoring

--- js/filter.js
+++ js/filter.js
@@ -4,53 +4,101 @@ $.fn.scrollEnd = function(callback, timeout) {
     if ($this.data('scrollTimeout')) {
       clearTimeout($this.data('scrollTimeout'));
     }
-    $this.data('scrollTimeout', setTimeout(callback,timeout));
+    $this.data('scrollTimeout', setTimeout(callback, timeout));
   });
 };
 
-function getParentTweetLi(elem) {
-    return elem.parentElement.parentElement.parentElement;
-};
-function addHideMark(elem) {
-    let tweetLi = getParentTweetLi(elem);
-    elem.className += " hide-k4eRo0";
-    tweetLi.className += " hide-k4eRo0";
-};
-function addCheckedMark(elem) {
-    elem.className += " checked-k4eRo0";
+function appendClass(elem, className) {
+    elem.className += ` ${className}`;
 }
 
-class FilterBlocker {
+function getAncestorElem(elem, generationNum) {
+    for (let i = 0; i < generationNum; i++) {
+        elem = elem.parentElement;
+    }
+    return elem;
+}
+
+class TweetMarkManager {
+    constructor() {
+        // Marks don't have meanings at all, but exist to identify.
+        this.HIDDEN_MARK = "hidden-k4eRo0"
+        this.CHECKED_MARK = "checked-k4eRo0"
+    }
+
+    excludeMarkedTweets($tweets) {
+        return $tweets.not(`.${this.CHECKED_MARK}`);
+    }
+
+    appendCheckedMark(tweet) {
+        appendClass(tweet, this.CHECKED_MARK);
+    }
+
+    appendHiddenMark(tweet) {
+        appendClass(tweet, this.HIDDEN_MARK);
+
+        // Hide wrapper, too.
+        let tweetWrapper = this.getTweetWrapper(tweet);
+        appendClass(tweetWrapper, this.HIDDEN_MARK);
+    }
+
+    getTweetWrapper(tweet) {
+        return getAncestorElem(tweet, 3);
+    }
+}
+
+class TweetContentInspector {
     constructor(filterRegex) {
         this.filterRegex = filterRegex;
     }
-    filterAllTweets() {
-        var i = 0;
-        let tweets = $(".tweet-text").not(".hide-k4eRo0").not(".checked-k4eRo0");
-        tweets.each((i, elem) => {
-            let text = elem.innerText;
-            if (this.filterRegex.test(text)) {
-                addHideMark(elem);
-            } else {
-                addCheckedMark(elem);
+
+    detect(text) {
+        return this.filterRegex.test(text);
+    }
+}
+
+class TweetWalker {
+    constructor(filterRegex) {
+        this.markManager = new TweetMarkManager();
+        this.tweetContentInspector = new TweetContentInspector(filterRegex);
+    }
+
+    getAllTweets() {
+        return $(".tweet-text");
+    }
+
+    walk() {
+        let $tweets = this.markManager.excludeMarkedTweets(this.getAllTweets());
+        $tweets.each((__, tweet) => {
+            let tweetTextContent = tweet.innerText;
+
+            if (this.tweetContentInspector.detect(tweetTextContent)) {
+                this.markManager.appendHiddenMark(tweet);
             }
+
+            this.markManager.appendCheckedMark(tweet);
         });
     }
 }
 
-let regex = chrome.storage.sync.get({
+// Main Process
+chrome.storage.sync.get({
         regex: "Delete this. And then input text you want to filter by harukaeru"
     }, function(items) {
-        let re = new RegExp(items.regex);
-        let filterBlocker = new FilterBlocker(re);
-        filterBlocker.filterAllTweets();
+        let filterRegex = new RegExp(items.regex);
+        let tweetWalker = new TweetWalker(filterRegex);
+
+        // Alias
+        let walk = tweetWalker.walk();
+
+        walk();
 
         $("#timeline").on('click', function() {
-            filterBlocker.filterAllTweets();
+            walk();
         });
 
         $(window).scrollEnd(function(){
-            filterBlocker.filterAllTweets();
+            walk();
         }, 100);
     }
 );
